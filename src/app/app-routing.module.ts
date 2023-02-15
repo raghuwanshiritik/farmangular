64a132ffa6c6ec0fdd80c1c6f22ca9ee71684802
ritikraghuwanshi <ritikraghuwanshi987@gmail.com>
2023-02-15 23:05:47 +0530

    farmproject

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
+import { ContactComponent } from './contact/contact.component';
+import { ProductComponent } from './product/product.component';
+import { ServiceComponent } from './service/service.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {
+  path:'',
+  component:HomeComponent
+ },
+ {
+  path:'about',
+  component:AboutComponent
+ },
+ {
+  path:'service',
+  component:ServiceComponent
+ },
+ {
+  path:'product',
+  component:ProductComponent
+ },
+ {
+  path:'contact',
+  component:ContactComponent
+ }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServiceComponent } from './service/service.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent,
+    ServiceComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
